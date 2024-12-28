e51d1473869420acee33e46a3748a08e969d9faf
82109 <82109@192.168.219.106>
2024-12-28 17:23:39 +0900

    first commit in devC

--- StudyGit6/src/B.java
+++ StudyGit6/src/B.java
@@ -3,7 +3,7 @@ public class B {
 	public static void main(String args[]) {
 		
 		System.out.println("Jae");
-	
+		System.out.println("Last Hong");
 	}
 
 
--- StudyGit6/src/C.java
+++ StudyGit6/src/C.java
@@ -3,7 +3,7 @@ public class C {
 	public static void main(String args[]) {
 		
 		System.out.println("Hong");
-	
+		System.out.println("First Ryu");
 	}
 
 }
